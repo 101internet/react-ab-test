58d0405e34a82b3b4713b50c80e73dc4661473f8
xfloooo <xfloooo1@gmail.com>
2018-10-29 15:38:40 +0300

    remove js-coockie from localstorage example

--- examples/client_render_localstorage/application/index.js
+++ examples/client_render_localstorage/application/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 import { AB_Provider, AB_Test, StorageAdapter } from "../../../dist";
-import Cookies from "js-cookie";
 
 const experimentMap = [
   {
